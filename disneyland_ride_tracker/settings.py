8bba419b5bfbf5e1622acb66816d542144edaca7
Patrick Walsh <paddybob39@gmail.com>
2024-12-03 01:16:01 +0000

    set debug to true

--- disneyland_ride_tracker/settings.py
+++ disneyland_ride_tracker/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-patrick5009-disneylandr-fhezgyfw2gq.ws.codeinstitute-ide.net', 'disneyland-ride-tracker-07b2f49cf7e9.herokuapp.com']
 
