e8b0fc14cd0e02033f455953d270d7896b40a187
Patrick Walsh <paddybob39@gmail.com>
2024-12-06 17:06:37 +0000

    create view function

--- ride_tracker/views.py
+++ ride_tracker/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 # Create your views here.
 
-class HomePage(TemplateView):
-    """
-    Displays home page"
-    """
-    template_name = 'index.html'
+def index(request):
+    return HttpResponse('Hello World')
 
