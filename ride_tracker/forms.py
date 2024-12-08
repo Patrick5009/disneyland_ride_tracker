d9acb92aeda0f2041270ef1f2f2cc682bdbbfbaf
Patrick Walsh <paddybob39@gmail.com>
2024-12-08 00:18:26 +0000

    create form.py class, views.py function, rides.html POST form

---
+++ ride_tracker/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.forms import ModelForm
+
+from .models import *
+
+
+class RideForm(forms.ModelForm):
+    
+    class Meta:
+        model = Ride
+        fields = ['ride_name']
--- ride_tracker/views.py
+++ ride_tracker/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import *
+from .forms import *
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'ride_tracker/rides.html')
+    ride_tracker = Ride.objects.all()
+
+    form = RideForm()
+
+    if request.method == 'POST':
+        form = RideForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'ride_tracker':ride_tracker, 'form':form}
+    return render(request, 'ride_tracker/rides.html', context)
 
