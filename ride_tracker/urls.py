644da12982af124d665805b0677dd23ef394533c
Patrick Walsh <paddybob39@gmail.com>
2024-12-08 19:45:05 +0000

    update urls.py file with path for delete

--- ride_tracker/urls.py
+++ ride_tracker/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='rides'),
     path('update_ride/<str:pk>/', views.updateRide, name="update_ride"),
+    path('delete/<str:pk>/', views.deleteRide, name="delete"),
 
 ]
