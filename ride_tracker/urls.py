15bbfaff1e7a471107f25d3f7ebfec8677de8360
Patrick Walsh <paddybob39@gmail.com>
2024-12-08 18:41:13 +0000

    include update_ride view in urls.py, pass in str

--- ride_tracker/urls.py
+++ ride_tracker/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',
-        views.index, name='index'),
+    path('', views.index, name='index'),
+    path('update_ride/<str:pk>/', views.updateRide, name="update_ride"),
+
 ]
