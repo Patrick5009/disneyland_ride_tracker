1c3222c6df9e466849fd4907db77ce7f97f94182
Patrick Walsh <paddybob39@gmail.com>
2024-12-10 15:05:32 +0000

    set site id=1, lodin redirects to home,middeware for allauth,account email verification to none and migrated

--- disneyland_ride_tracker/settings.py
+++ disneyland_ride_tracker/settings.py
@@ -54,6 +54,10 @@ INSTALLED_APPS = [
     'ride_tracker',
 ]
 
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -63,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'disneyland_ride_tracker.urls'
@@ -119,6 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
