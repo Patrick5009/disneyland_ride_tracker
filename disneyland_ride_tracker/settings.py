d2c2672dcce633764e7f8432da4dd57b63757554
Patrick Walsh <paddybob39@gmail.com>
2024-12-11 18:40:19 +0000

    update view functions with @login required and filter request, add LOGIN_URL = /accounts/login/ to settings.py

--- disneyland_ride_tracker/settings.py
+++ disneyland_ride_tracker/settings.py
@@ -57,6 +57,8 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/accounts/login/'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
